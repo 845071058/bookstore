eed0ee07225b5bc707ebbc178f5d8402b41299b8
hexian209 <845071058@qq.com>
2016-10-06 15:37:28 +0800

    第二次

---
+++ bookstore-dao/src/main/java/com/hexian/dao/HexianImpl.java
@@ -0,0 +1,7 @@
+package com.hexian.dao;
+
+/**
+ * Created by Administrator on 2016/10/6.
+ */
+public class HexianImpl {
+}
---
+++ bookstore-web/src/main/java/com/hexian/web/UserController.java
@@ -0,0 +1,7 @@
+package com.hexian.web;
+
+/**
+ * Created by Administrator on 2016/10/6.
+ */
+public class UserController {
+}
